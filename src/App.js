5e74fa125d04a1fc480dc791d1c4494aacf03f32
Victor Mora <victorlvmora@gmail.com>
2022-12-18 16:39:41 -0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function Bemvindo(props) {
+  return <h2>Bem vindo {props.nome}</h2>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bemvindo nome="Victor" />
+      <Bemvindo nome="Luana" />
+      <Bemvindo nome="Ana" />
     </div>
   );
+
 }
 
 export default App;
